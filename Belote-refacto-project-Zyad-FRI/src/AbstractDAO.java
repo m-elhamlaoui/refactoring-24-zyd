9a534efc5697f1eb503f0a587aa3076a766106d9
FRI ZYAD <149961163+ZyadFri@users.noreply.github.com>
2025-01-09 22:55:36 +0100

    Delete Belote-refacto-project-Zyad-FRI/src directory

--- Belote-refacto-project-Zyad-FRI/src/AbstractDAO.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public interface AbstractDAO<T> {
-    T get(int id);
-    List<T> getAll();
-    void update(T t);
-    void delete(int id);
-}
--- Belote-refacto-project-Zyad-FRI/src/IMatchGenerator.java
+++
@@ -1,5 +0,0 @@
-import java.util.Vector;
-
-interface IMatchGenerator {
-    Vector<Vector<Match>> generateMatches(Vector<Equipe> equipes);
-}
